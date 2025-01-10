70312eeca471e8babb08eeefb9fb6fc1b9027942
Charles Giese <cmggiese@gmail.com>
2025-01-10 14:31:06 +0100

    solved day4, part 2

--- aoc_04/src/main.rs
+++ aoc_04/src/main.rs
@@ -45,11 +45,39 @@ fn have_xmas(lines: Vec<Vec<char>>, i: i32, j: i32, d: &(i32, i32), n: usize, m:
     return 1;
 }
 
+fn have_mas_x(lines: Vec<Vec<char>>, i: i32, j: i32, n: usize, m: usize) -> i32 {
+    if !(1 <= i
+        && i < <usize as TryInto<i32>>::try_into(n).unwrap() - 1
+        && 1 <= j
+        && j < <usize as TryInto<i32>>::try_into(m).unwrap() - 1)
+    {
+        return 0;
+    }
+    let ii = i as usize;
+    let jj = j as usize;
+
+    if lines[ii as usize][jj] != 'A' {
+        return 0;
+    }
+
+    let diag_1 = format!("{}{}", lines[ii - 1][jj - 1], lines[ii + 1][jj + 1]);
+    let diag_2 = format!("{}{}", lines[ii - 1][jj + 1], lines[ii + 1][jj - 1]);
+
+    let array: [&str; 2] = ["MS", "SM"];
+
+    if array.contains(&diag_1.as_str()) && array.contains(&diag_2.as_str()) {
+        return 1;
+    }
+
+    return 0;
+}
+
 fn main() {
     let lines = read_lines("input.txt");
     let matrix = populate_matrix(lines);
 
     let mut count = 0;
+    let mut count2 = 0;
 
     let n = matrix.len();
     let m = matrix[0].len();
@@ -76,7 +104,15 @@ fn main() {
                     m,
                 );
             }
+            count2 += have_mas_x(
+                matrix.clone(),
+                i.try_into().unwrap(),
+                j.try_into().unwrap(),
+                n,
+                m,
+            );
         }
     }
     println!("{:?}", count);
+    println!("{:?}", count2);
 }
